17363daac03b9dc51cf18cc3528579dc7245b4d5
AlgosStile <oleg.todor@mail.ru>
2024-06-01 08:31:32 +0300

    added ya search

---
+++ src/test/java/org/selenium/allure/pages/YandexSearchPage.java
@@ -0,0 +1,24 @@
+package org.selenium.allure.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class YandexSearchPage extends BasicPage {
+
+    @FindBy(how = How.ID, using = "text")
+    private WebElement searchInput;
+
+    @FindBy(how = How.XPATH, using = "//button[@type='submit']")
+    private WebElement searchButton;
+
+    public YandexSearchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void searchFor(String text) {
+        searchInput.sendKeys(text);
+        searchButton.click();
+    }
+}
